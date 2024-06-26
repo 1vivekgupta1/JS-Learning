4ecc905f6d31c765f2a99c297a86e8d98ab34ec0
1vivekgupta1 <01vivekgupta.1@gmail.com>
2024-06-26 17:53:29 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" ; // treat all JS code as newer version default js is coded in newer format only
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3); // code readability should be high
+
+console.log("Vivek");
+
+let name = "Vivek"
+let age = 21
+let isLoggedIn =  false
+let state ;
+// number => 0 - 2^53 -1
+//bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); // object
