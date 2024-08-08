748c7b900aef1b923e12e9256c7a215fb027e959
1vivekgupta1 <01vivekgupta.1@gmail.com>
2024-08-08 11:39:34 +0000

    done truthsy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -26,3 +26,29 @@ if(Object.keys(emptyObj).length === 0){
     console.log("Object is empty");
     
 }
+
+// Nullish Coalescing Operator (??): null undefined
+
+// The nullish coalescing (??) operator is a logical operator that returns its right-hand side operand when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand.
+
+
+
+
+let val1;
+// val1 = 5?? 10
+// val1= null ?? 10
+// val1 = undefined ?? 15
+
+val1 =  null ?? 10 ?? 20
+ console.log(val1);
+ 
+
+
+ // Ternary Operator
+
+ // conditon? true: false
+
+ const iceTeaPrice = 100
+
+ iceTeaPrice<=80? console.log("less than 80") : console.log("more than 80");
+  
